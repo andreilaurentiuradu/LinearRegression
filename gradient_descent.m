38b0627eb877ac1bfd2f67282457512b733d1aa5
andreilaurentiuradu <117320481+andreilaurentiuradu@users.noreply.github.com>
2023-04-19 15:14:23 +0300

    Add files via upload

---
+++ gradient_descent.m
@@ -0,0 +1,22 @@
+function [Theta] = gradient_descent(FeatureMatrix, Y, n, m, alpha, iter)
+
+
+##  FeatureMatrix
+##  Y
+##  n
+##  m
+##  alpha
+##  iter
+
+  % Inițializarea vectorului de coeficienți
+  Theta = zeros(n,1);
+
+  % Calcularea gradientului
+  % actualizarea vectorului de coeficienți în fiecare iterație
+  for i = 1:iter
+      gradient = (1/m) * FeatureMatrix' * (FeatureMatrix*Theta - Y);
+      Theta = Theta - alpha * gradient;
+  endfor
+
+
+endfunction
