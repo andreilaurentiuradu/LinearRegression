6911b9543edb29fe18b1e7f57cbc512c3cac0266
andreilaurentiuradu <117320481+andreilaurentiuradu@users.noreply.github.com>
2023-05-04 00:39:39 +0300

    gata

--- prepare_for_regression.m
+++ prepare_for_regression.m
@@ -7,9 +7,10 @@ function [FeatureMatrix] = prepare_for_regression(InitialMatrix)
   FeatureMatrix = zeros(m, n);
 
   % parcurgem matricea pe coloane
+  k = 1;
   for j = 1:n
     % daca primul element de pe coloana este string
-    if ischar(InitialMatrix{1, j})
+    if ischar(InitialMatrix{1, j}(1))
       % daca este de tip yes/no
       if strcmp(InitialMatrix{1, j},'yes') || strcmp(InitialMatrix{1, j},'no')
         for i = 1:m
@@ -24,27 +25,29 @@ function [FeatureMatrix] = prepare_for_regression(InitialMatrix)
         % cream o noua coloana
         for i = 1:m
             if strcmp(InitialMatrix{i, j},'unfurnished')
-              FeatureMatrix(i, j) = 0;
-              FeatureMatrix(i, j+1) = 1;
+              FeatureMatrix(i, k) = 0;
+              FeatureMatrix(i, k+1) = 1;
             endif
             if strcmp(InitialMatrix{i, j},'semi-furnished')
-              FeatureMatrix(i, j) = 1;
-              FeatureMatrix(i, j+1) = 0;
+              FeatureMatrix(i, k) = 1;
+              FeatureMatrix(i, k+1) = 0;
             endif
             if strcmp(InitialMatrix{i, j},'furnished')
-              FeatureMatrix(i, j) = 0;
-              FeatureMatrix(i, j+1) = 0;
+              FeatureMatrix(i, k) = 0;
+              FeatureMatrix(i, k+1) = 0;
             endif
         endfor
         % am creat o noua coloana
         n++;
-        j++;
+        k++;
       endif
     else
       % daca este de numar pastram valoarea din matricea initiala
       for i = 1:m
-          FeatureMatrix(i, j) = InitialMatrix{i, j};
+          FeatureMatrix(i, k) = InitialMatrix{i, j};
       endfor
     endif
+    k++;
   endfor
+  
 endfunction
