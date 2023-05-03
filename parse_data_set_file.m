6d7b9cd42bfc5ada6a4be5b8bcf27abe00e793ba
andreilaurentiuradu <117320481+andreilaurentiuradu@users.noreply.github.com>
2023-05-03 14:14:38 +0300

    merge pe checker

--- parse_data_set_file.m
+++ parse_data_set_file.m
@@ -7,7 +7,7 @@ function [Y, InitialMatrix] = parse_data_set_file(file_path)
   m = dimensions(1);
   n = dimensions(2);
 
-  % declaram o matrice de cell(sa putem retine si stringuri si numere
+  % declaram o matrice de cell(sa putem retine si stringuri si numere)
   InitialMatrix = cell(m, n);
 
   % initializam vectorul coloana
@@ -19,16 +19,13 @@ function [Y, InitialMatrix] = parse_data_set_file(file_path)
   % stocam in matrice predicatorii si vectorul de iesire
   for i = 1:m
 
-    % citim elementul din vectorul de iesire
-    Y(i, 1) = fscanf(file, '%lf');# posibil sa iti trebuiasca cast dupa
-
-    % citim restul liniei
+    % citim linia
     line = fgets(file);
     % luam valorile din line si le punem in values avand ' ' separator
     values = strsplit(line, ' ');
 
-    for j = 1:n
-      %extrage valorile si le adauga in matrice in functie de tipul lor
+    for j = 1:n+1
+      %extragem valorile si le adaugam in matrice in functie de tipul lor
       if isletter(values{j}(1))
         InitialMatrix{i, j} = strtrim(values{j});
       else
@@ -38,7 +35,11 @@ function [Y, InitialMatrix] = parse_data_set_file(file_path)
     endfor
 
   endfor
+  Y = InitialMatrix(:, 1);
+  InitialMatrix(:, 1) = [];
 
+
+  Y = cell2mat(Y);
 
   % Close the file
   fclose(file);
